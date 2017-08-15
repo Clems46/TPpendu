16b94b68c6a876456746f25cc83f250d63eb2a9c
Clems46 <clems46@laposte.net>
2017-08-15 11:27:04 +0200

    Add foncion listeMots(), these fonction return a word

---
+++ donnees.py
@@ -0,0 +1,7 @@
+#Commentaires??
+import random
+def listeMots():
+    a = ['igloo', 'poisson', 'matin', 'bus', 'voiture', 'moto', 'python', 'bateau', 'maison']
+    nombreDeMots = 9
+    return a[random.randint(0, nombreDeMots)]
+
