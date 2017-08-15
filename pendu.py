207588b46a50f3c269a6edfca58ef30c0dce51e0
Clems46 <clems46@laposte.net>
2017-08-15 19:38:38 +0200

    Scores calculation in place

--- pendu.py
+++ pendu.py
@@ -24,48 +24,60 @@ else:
     b = ReturnScore(Name)
     print ('Bienvenue', Name, ', ton score est de ', b)
 
+play = 1
+
+while play == 1:
+
 #Choix du mot
 
-mot = listeMots()
+    mot = listeMots()
 
-motCache = ''
-for lettres in mot: #Créer un mot avec des étoiles avec la meme longueur
-                    #que le mot à deviner
-    motCache = motCache + '*'
-
-
-
-i = NbrofChances()
-coup = 0
-MotTrouve = 0
+    motCache = ''
+    for lettres in mot: #Créer un mot avec des étoiles avec la meme longueur
+                        #que le mot à deviner
+        motCache = motCache + '*'
 
 
-    
-while MotTrouve >= 0 and coup < NbrofChances():
+
+    i = NbrofChances()
+    coup = 0
+    MotTrouve = 0
+
+
         
-    print('Mot à deviner : ', motCache)
-    lettreJoueur = input('Entrer une lettre :')
-    a = mot.find(lettreJoueur)
-    if a < 0:
-        print('Il n\' a pas de {} dans le mot'.format(lettreJoueur))
-        coup += 1
-        print('Coup restant : {} / {}'.format(coup, NbrofChances()))
+    while MotTrouve >= 0 and coup < NbrofChances():
+            
+        print('Mot à deviner : ', motCache)
+        lettreJoueur = input('Entrer une lettre :')
+        a = mot.find(lettreJoueur)
+        if a < 0:
+            print('Il n\' a pas de {} dans le mot'.format(lettreJoueur))
+            coup += 1
+            print('Coup restant : {} / {}'.format(coup, NbrofChances()))
 
-    else :
-        print("Bien")
-        print("")
+        else :
+            print("Bien")
+            print("")
 
-        while a >= 0 & a <= len(mot):      #Boucle qui permet d'ajouter plusieurs lettres identiques du meme mots           
-            motCache = ModifyString(motCache, a, lettreJoueur)
-            a += 1
-            a = mot.find(lettreJoueur, a, len(mot))
-                
-    MotTrouve = motCache.find('*')      #Cherche des étoiles dans le mot cachés
-#afin de checker si le mot est trouvé
+            while a >= 0 & a <= len(mot):      #Boucle qui permet d'ajouter plusieurs lettres identiques du meme mots           
+                motCache = ModifyString(motCache, a, lettreJoueur)
+                a += 1
+                a = mot.find(lettreJoueur, a, len(mot))
+                    
+        MotTrouve = motCache.find('*')      #Cherche des étoiles dans le mot cachés
+                                            #afin de checker si le mot est trouvé
+    # Calcul des scores
+    scoreJoueur = ReturnScore(Name)
+    scoreJoueur = scoreJoueur + NbrofChances() - coup
+    changeScores(Name, scoreJoueur)
 
-if MotTrouve < 0:
-    print('Trouvé !!')
-if coup >= i:
-    print('Perdu !!!')
-
+    if MotTrouve < 0:
+        print('Trouvé !!')
+    if coup >= i:
+        print('Perdu !!!')
+    print('{}, votre score est de {}'.format(Name, ReturnScore(Name)))
+    
+    
+    playStr = input('Play again ? 0 ou 1 :')
+    play = int(playStr)
 
